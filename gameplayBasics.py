fd0ac0b4501ea1879ef8f42bad7ade8cc1c7372a
Korde Carter <kordecarter@gmail.com>
2025-03-27 21:49:34 -0500

    Layed the groundwork for the card class

--- gameplayBasics.py
+++ gameplayBasics.py
@@ -1,2 +1,56 @@
-class Card:
-    pass
+class Cards:
+    def __init__(self, name = "default", power = 0):
+        self.name = name
+        self.power = power
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if(isinstance(value, str)):
+            self._name = value
+        else:
+            self._name = "default"
+
+    @property
+    def power(self):
+        return self._power
+    
+    @power.setter
+    def power(self, value):
+        if(isinstance(value, int)):
+            self._power = value
+        else:
+            self._power = 0
+
+    def __str__(self):
+        return f"{self.name} has {self.power} power"
+    
+    def __gt__(self, other):
+        card1 = self.power
+        card2 = other.power
+        if (card1 > card2):
+            return True
+        return False
+    
+    def __lt__(self, other):
+        card1 = self.power
+        card2 = other.power
+        if (card1 < card2):
+            return True
+        return False
+    
+    def __eq__(self, other):
+        card1 = self.power
+        card2 = other.power
+        if(card1 == card2):
+            return True
+        return False
+    
+c1 = Cards()
+c2 = Cards("guy", 7)
+print(c1) 
+print(c2)
+print (c1 > c2)
