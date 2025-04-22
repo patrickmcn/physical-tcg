d4390883672ebad79e3cde8abf05db269cdb05cb
Collin Tucker <cbt023@latech.edu>
2025-04-22 00:10:52 -0500

    Added more Card Effects

--- CardEffects.py
+++ CardEffects.py
@@ -1,5 +1,14 @@
-def EnergyDrink(targetCard, amount = 2):
-    if targetCard and hasattr(targetCard, 'power'):
-        targetCard.power += amount
-        print(f"{targetCard.name}'s power increased by {amount} to {targetCard.power}")
+def EnergyDrink(target, amount = 2):
+    if target and hasattr(target, 'power'):
+        target.power += amount
+        print(f"{target.name}'s power increased by {amount} to {target.power}")
         
+def Caltrops(target, amount = 2):
+    if target and hasattr(target, 'power'):
+        target.power -= amount
+        print(f"{target.name}'s power decreased by {amount} to {target.power}")
+def AndresEffect(currentLane, currentPlayer, lanes, card, Buff= 3):
+    emptyLanes= all(lane[currentPlayer] is None or laneIndex == currentLane for laneIndex, lane in enumerate(lanes) if laneIndex != currentLane)
+    if emptyLanes:
+        card.power += Buff
+        print(f"{card.name} is alone, he gains {Buff} power and is ever stronger at {card.power}")
